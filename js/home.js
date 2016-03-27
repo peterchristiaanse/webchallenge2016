02bca00a50a5d7a744c9949a8d1c8a9fc8552783
rafaelklaessen <rafaelklaessen.nl@gmail.com>
2016-03-27 12:21:10 +0200

    backup

--- js/home.js
+++ js/home.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 console.log("Home.js working.");
 
 $(document).ready(function(){
   $('h1').tell();
 });
-=======
-console.log("Home.js working.");
-
-$(document).ready(function(){
-  $('h1').tell();
-});
->>>>>>> 3174284a3d8764da7f341dcdebf16d41ce9b00d7
--- js/main.js
+++ js/main.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 console.log("Main.js working.");
-=======
-console.log("Main.js working.");
->>>>>>> 3174284a3d8764da7f341dcdebf16d41ce9b00d7
